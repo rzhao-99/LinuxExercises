547dec58701f57312c538f799ac8f5622ff273fc
Ran Zhao <rainbowqwertyui@gmail.com>
2022-10-17 10:41:43 -0500

    school.sh version 1

--- school.sh
+++ school.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d, -f 7 | 
+    { sum=0;
+     count=0;
+     while read value;
+     do sum=$(($sum + $value));
+        count=$(($count+1));
+     done; 
+     avg=$(echo "scale=6; $sum/$count" | bc);
+     echo $avg;
+    }
